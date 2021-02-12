854e4cb2cd6cc52b225ad3078780b1f5d49347f9
Joon Kyo Kyo <600020209@fjuhsd.org>
2021-02-12 01:24:21 +0000

    Completed Number class with all methods

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Joon Kyo Kim
 
 
 //write the Number class
@@ -9,20 +9,63 @@
 //to guide you
 
 //header for Number class
+public class Number{
+  //private instance variable (must be an Integer)
+  private Integer num;
+  //constructor
+  public Number(){
+    num = 0;
+  }
+  public Number(Integer n){
+    num = n;
+  }
+  //getter method
+  public Integer getNum(){
+    return num;
+  }
 
-//private instance variable (must be an Integer)
+  //setter method
+  public void setNum(Integer newNum){
+    num = newNum;
+  }
+  //isOdd method
+  public boolean isOdd(){
+    //If the modulo of this number and 2 is 0:
+    if(num % 2 == 0){
+      //Return false
+      return false;
+    //End if
+    }
+    //Return true
+    return true;
+  }
 
-//constructor
+  //isPerfect method
+  public boolean isPerfect(){
+    //Initialize int variable sum with 0
+    int sum = 0;
+    //Start for loop; intializing int variable i with value 1, while i is less than this number, and incrementing i by one each run:
+    for(int i = 1; i < num; i++){
+      //If the modulo of this number and i is 0:
+      if(num % i == 0){
+        //Add i to sum
+        sum += i;
+      //End if
+      }
+    //End for
+    }
+    //If sum is equal to this number:
+    if(sum == num){
+      //Return true
+      return true;
+    //End if
+    }
+    //Return false
+    return false;
+  }
 
-
-//getter method
-
-
-//setter method
-
-//isOdd method
-
-//isPerfect method
-
-
-//toString
+  //toString
+  public String toString(){
+    return "Number: " + num;
+  }
+}
